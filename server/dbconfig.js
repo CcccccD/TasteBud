2fc2de3821bc3a045f80d08fff1961221d796cc3
yaochao <1257035408@qq.com>
2017-10-11 21:26:59 +0800

    美食网站前端和后台

---
+++ server/dbconfig.js
@@ -0,0 +1,12 @@
+/**
+ * Created by yaochao on 2017/8/25.
+ */
+var options={
+    host:'127.0.0.1',
+    port:'3306',
+    user:'root',
+    password:'yaochao520',
+    database:'tastebud'
+};
+
+exports.options=options;
